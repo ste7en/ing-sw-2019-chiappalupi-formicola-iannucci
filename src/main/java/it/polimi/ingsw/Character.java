3ee905d6a3ee521ebee8fa132b36b5023fbe09cf
Stefano Formicola <stefano_17sf@libero.it>
2019-04-04 21:16:27 +0200

    Character class and tests

--- src/main/java/it/polimi/ingsw/Character.java
+++ src/main/java/it/polimi/ingsw/Character.java
@@ -3,55 +3,76 @@ package it.polimi.ingsw;
 import java.util.*;
 
 /**
- * 
+ *
+ * @author Stefano Formicola
  */
 public class Character {
 
     /**
-     * Default constructor
+     * String passed as message of IllegalArgumentException when an empty string
+     * is passed as name parameter of Character's constructor
      */
-    public Character() {
-    }
+    private static final String EMPTY_STRING_NAME = "Cannot create a character with an empty string as name.";
 
     /**
-     * 
+     * String passed as message of IllegalArgumentException when an empty string
+     * is passed as description parameter of Character's constructor
      */
-    private String name;
+    private static final String EMPTY_STRING_DESCRIPTION = "Cannot create a character with an empty string as description.";
+
 
     /**
-     * 
+     * Color of the character
      */
     private PlayerColor color;
 
     /**
-     * 
+     * Description of the character
      */
     private String description;
 
-
+    /**
+     * Character's name
+     */
+    private String name;
 
     /**
-     * @return
+     * Constructor: creates a new Character based on its name, color and description.
+     * @param name the name of the character
+     * @param color the color of the character, defined as a PlayerColor Enum value
+     * @param description the description of the character, as defined in the game's manual
+     */
+    public Character(String name, PlayerColor color, String description) {
+        if (name.isEmpty()) { throw new IllegalArgumentException(EMPTY_STRING_NAME); }
+        if (description.isEmpty()) { throw new IllegalArgumentException(EMPTY_STRING_DESCRIPTION); }
+
+        this.name = name;
+        this.color = color;
+        this.description = description;
+    }
+
+    /**
+     * Returns the name of the character
+     * @return the name of the character
      */
     public String getName() {
-        // TODO implement here
-        return "";
+        return this.name;
     }
 
     /**
-     * @return
+     * Returns the color of the character
+     * @return the color of the character
      */
     public PlayerColor getColor() {
-        // TODO implement here
-        return null;
+        return this.color;
     }
 
     /**
-     * @return
+     * Returns the description of the character
+     * @return the description of the character
      */
     public String getDescription() {
-        // TODO implement here
-        return "";
+        return this.description;
     }
 
 }
