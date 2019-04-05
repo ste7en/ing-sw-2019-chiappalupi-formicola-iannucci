379ea0531645e0bdf4147d410dfc17313fcc50d2
Elena Iannucci <elena.iannucci@mail.polimi.it>
2019-04-06 00:04:49 +0200

    Some classes were uploaded

--- src/test/java/it/polimi/ingsw/CellTest.java
+++ src/test/java/it/polimi/ingsw/CellTest.java
@@ -1,13 +1,30 @@
 package it.polimi.ingsw;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Random;
 import static junit.framework.TestCase.assertTrue;
 
 public class CellTest {
-    @Test
-    public void play() {
-        assertTrue(true);
+
+    private Cell TestCell;
+    private static ArrayList<AmmoTile> ammoTiles  = new ArrayList<>();
+    private static ArrayList<Border> borders = new ArrayList<Border>();
+    private static ArrayList<AmmoColor> colors;
+
+    @BeforeClass
+    public static void setUpBeforeClass(){
+        Random rand = new Random();
+        for (int i=0; i<4; i++){
+            borders.add(Border.values()[new Random().nextInt(4)]);
+        }
+        CellColor color= CellColor.values()[new Random().nextInt(5)];
+
+        ammoTiles.add(new AmmoTile(colors, new Random().nextBoolean() ? null : new Powerup("name", "description", AmmoColor.values()[new Random().nextInt(3)])));
+
+
     }
 
 }
