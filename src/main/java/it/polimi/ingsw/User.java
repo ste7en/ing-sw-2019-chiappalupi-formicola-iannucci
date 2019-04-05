44d6a716f57799be89738adc932042840241b296
Daniele Chiappalupi <daniele.chiappalupi@mail.polimi.it>
2019-04-05 20:11:05 +0200

    Added UserTest.java and added javadoc to User.java

--- src/main/java/it/polimi/ingsw/User.java
+++ src/main/java/it/polimi/ingsw/User.java
@@ -2,20 +2,43 @@ package it.polimi.ingsw;
 
 import java.util.*;
 
+/**
+ *
+ * @author  Daniele Chiappalupi
+ */
 public class User {
 
+    /**
+     * ID of the user
+     */
     private final UUID userID;
+
+    /**
+     * Username of the user
+     */
     private final String username;
 
-    public User(UUID userID, String username) {
-        this.userID = userID;
+    /**
+     * Constructor: creates a new user from a username and generating a random UUID
+     * @param username
+     */
+    public User(String username) {
         this.username = username;
+        userID = UUID.randomUUID();
     }
 
+    /**
+     * User's ID getter
+     * @return the ID of the user
+     */
     public UUID getUserID() {
         return userID;
     }
 
+    /**
+     * User's username getter
+     * @return the username of the user
+     */
     public String getUsername() {
         return username;
     }
