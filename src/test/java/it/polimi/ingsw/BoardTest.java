0a20a6db1158e063006ab35028c14415f3e0c934
Elena Iannucci <elena.iannucci@mail.polimi.it>
2019-04-08 15:22:12 +0200

    Some classes were updated

--- src/test/java/it/polimi/ingsw/BoardTest.java
+++ src/test/java/it/polimi/ingsw/BoardTest.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw;
 
 import org.junit.*;
+import sun.awt.image.ImageWatched;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -20,10 +21,13 @@ import java.util.Random;
 public class BoardTest {
 
     private Board boardTest;
-    private static HashMap<AmmoColor, ArrayList<Weapon>> weaponsdecks = new HashMap<>();
-    private static HashMap<Integer, ArrayList<PlayerColor>> skullsTrack = new HashMap<>();
     private static GameMap gameMap;
     private static LinkedHashMap<Player, Cell> playersPosition = new LinkedHashMap<>();
+    private static HashMap<AmmoColor, ArrayList<Weapon>> weaponsdecks = new HashMap<>();
+    private static ArrayList<Weapon> deck1 = new ArrayList<>();
+    private static ArrayList<Weapon> deck2 = new ArrayList<>();
+    private static ArrayList<Weapon> deck3 = new ArrayList<>();
+    private static LinkedHashMap<Integer, ArrayList<PlayerColor>> skullsTrack = new LinkedHashMap<>();
     private static DecksHandler decksHandler;
     private static ArrayList<Weapon> weapons = new ArrayList<>();
     private static ArrayList<AmmoTile> ammoTiles  = new ArrayList<>();
@@ -76,19 +80,16 @@ public class BoardTest {
 
         gameMap = new GameMap(MapType.conf_4, playersPosition);
 
-        ArrayList<Weapon> deck1 = new ArrayList<>();
         for(int i=0; i<3; i++){
             deck1.add(decksHandler.drawWeapon());
         }
         weaponsdecks.put(AmmoColor.blue, deck1);
 
-        ArrayList<Weapon> deck2 = new ArrayList<>();
         for(int i=0; i<3; i++){
             deck2.add(decksHandler.drawWeapon());
         }
         weaponsdecks.put(AmmoColor.red, deck2);
 
-        ArrayList<Weapon> deck3 = new ArrayList<>();
         for(int i=0; i<3; i++){
             deck3.add(decksHandler.drawWeapon());
         }
@@ -106,11 +107,86 @@ public class BoardTest {
     public void setUp() {
         boardTest = new Board(gameMap, weaponsdecks, skullsTrack);
     }
-/**
+
+    /**
+     * Tests the pickWeapon method
+     * @see Board#pickWeapon(Weapon)
+     */
     @Test
-    public void setBoardTest() {
-
+    public void testPickWeapon(){
+        boardTest.pickWeapon(deck1.get(0));
+        assertNull(boardTest.showWeapons(AmmoColor.blue).get(0));
+        for (int i=1; i<3; i++) {
+            assertNotNull(boardTest.showWeapons(AmmoColor.blue).get(i));
+        }
+        for (int i=0; i<3; i++) {
+            assertNotNull(boardTest.showWeapons(AmmoColor.yellow).get(i));
+        }
+        for (int i=0; i<3; i++) {
+            assertNotNull(boardTest.showWeapons(AmmoColor.red).get(i));
+        }
     }
-*/
 
+    /**
+     * Tests the refillWeapons method
+     * @see Board#refillWeapons(DecksHandler)
+     */
+    @Test
+    public void testRefillWeapons(){
+        boardTest.pickWeapon(deck1.get(0));
+        boardTest.refillWeapons(decksHandler);
+        for (int i=0; i<3; i++) {
+            assertNotNull(boardTest.showWeapons(AmmoColor.blue).get(i));
+        }
+        for (int i=0; i<3; i++) {
+            assertNotNull(boardTest.showWeapons(AmmoColor.yellow).get(i));
+        }
+        for (int i=0; i<3; i++) {
+            assertNotNull(boardTest.showWeapons(AmmoColor.red).get(i));
+        }
+    }
+
+    /**
+     * Tests the showWeapons method
+     * @see Board#showWeapons(AmmoColor)
+     */
+    @Test
+    public void testShowWeapons(){
+        assertEquals(deck1, boardTest.showWeapons(AmmoColor.blue));
+        assertEquals(deck2, boardTest.showWeapons(AmmoColor.red));
+        assertEquals(deck3, boardTest.showWeapons(AmmoColor.yellow));
+    }
+
+    /**
+     * Tests the skullsLeft method
+     * @see Board#skullsLeft()
+     */
+    @Test
+    public void testSkullsLeft(){
+        assertEquals(8, boardTest.skullsLeft());
+        boardTest.addBloodFrom(playerTest.getCharacter().getColor(), 2);
+        assertEquals(7, boardTest.skullsLeft());
+        for (int i=1;i<8; i++){
+            boardTest.addBloodFrom(playerTest2.getCharacter().getColor(), 1);
+        }
+        assertEquals(0, boardTest.skullsLeft());
+    }
+
+    /**
+     * Tests the addBloodFrom method
+     * @see Board#addBloodFrom(PlayerColor, Integer)
+     */
+    @Test
+    public void testAddBloodFrom (){
+        ArrayList<PlayerColor> blood = new ArrayList<>();
+        ArrayList<PlayerColor> blood2 = new ArrayList<>();
+        blood.add(PlayerColor.yellow);
+        blood.add(PlayerColor.yellow);
+        blood2.add(PlayerColor.blue);
+        boardTest.addBloodFrom(playerTest.getCharacter().getColor(), 2);
+        boardTest.addBloodFrom(playerTest2.getCharacter().getColor(), 1);
+        assertEquals(6, boardTest.skullsLeft());
+        assertEquals(blood, boardTest.getBlood(0));
+        assertEquals(blood2, boardTest.getBlood(1));
+    }
 }
