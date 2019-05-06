a8da9920c8287f9bc50637b3b90122cd54d3f2b4
Stefano Formicola <stefano_17sf@libero.it>
2019-05-06 14:13:27 +0200

    Abstract classes and interfaces of ConnectionHandler

---
+++ src/main/java/it/polimi/ingsw/networking/ConnectionHandler.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.networking;
+
+import it.polimi.ingsw.utility.Loggable;
+
+/**
+ * Abstract superclass of Server and Client's connectionHandlers.
+ * This implements ConnectionHandlerSenderDelegate's methods in order to implement
+ * sending methods through the network with a delegator pattern and a protected
+ * ConnectionHandlerReceiverDelegate instance attribute to process received messages.
+ *
+ * @author Stefano Formicola
+ */
+
+public abstract class ConnectionHandler implements ConnectionHandlerSenderDelegate, Loggable {
+    /**
+     * The port number used for the handled connection
+     */
+    protected Integer portNumber;
+
+    /**
+     * A delegate class used to handle received messages
+     */
+    protected ConnectionHandlerReceiverDelegate receiverDelegate;
+}
--- src/main/java/it/polimi/ingsw/networking/ConnectionHandlerSenderDelegate.java
+++ src/main/java/it/polimi/ingsw/networking/ConnectionHandlerSenderDelegate.java
@@ -1,5 +1,14 @@
 package it.polimi.ingsw.networking;
 
+/**
+ * Delegator interface used to send messages through
+ * the network and implemented by Client and Server classes.
+ *
+ * @author Stefano Formicola
+ */
 public interface ConnectionHandlerSenderDelegate {
+    /**
+     * Sends a message through its connection
+     */
     public void send();
 }
