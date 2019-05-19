22b6b193a4fe1e23fced75a2dc35e3c474544972
Stefano Formicola <stefano_17sf@libero.it>
2019-05-19 20:04:12 +0200

    Ping utility and Pingable interface

---
+++ src/main/java/it/polimi/ingsw/utility/Ping.java
@@ -0,0 +1,121 @@
+package it.polimi.ingsw.utility;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Class that manages the connectivity state of a connection.
+ * When a client doesn't respond after EXECUTION_PERIOD_MILLISEC ms,
+ * the server-side connection is closed.
+ *
+ * @author Stefano Formicola
+ */
+public class Ping {
+
+    /**
+     * Log strings
+     */
+    private static final String RUNTIME_EXC_PING = "A runtime exception has been thrown during ping.";
+
+    /**
+     * Period between a scheduled pings
+     */
+    private static final long EXECUTION_PERIOD_MILLISEC = 750;
+
+    /**
+     * Delay when executing a ping for the first time
+     */
+    private static final long EXECUTION_DELAY_MILLISEC = 250;
+
+    /**
+     * Collection that manages pings over time
+     */
+    private ConcurrentMap<Pingable, Boolean> pingHandler = new ConcurrentHashMap<>();
+
+    /**
+     * Singleton instance
+     */
+    private static Ping shared = new Ping();
+
+    /**
+     * Private constructor responsible for scheduling the ping command
+     */
+    private Ping() {
+        /**
+         * This lambda iterates over the pingHandler collection and pings every connection,
+         * setting each state in the collection as false, meaning that a PONG message hasn't
+         * been received yet. When a PONG message is received, the convenience didPong(int)
+         * method re-sets the state to true. If a PONG message hasn't been received after
+         * EXECUTION_PERIOD_MILLISEC ms, the connection is interrupted meaning the client
+         * isn't connected anymore.
+         */
+        new ScheduledThreadPoolExecutor(1)
+                .scheduleAtFixedRate(
+                        () -> {
+                            try {
+                                var pH = Ping.getInstance().getPingHandler();
+                                pH.forEach(
+                                        (p, b) -> {
+                                            if (!b) { remove(p); return; }
+                                            p.ping();
+                                            pH.replace(p, false);
+                                        }
+                                );
+                            } catch (RuntimeException e) {
+                                AdrenalineLogger.errorException(RUNTIME_EXC_PING, e);
+                            }
+
+                        },
+                        EXECUTION_DELAY_MILLISEC,
+                        EXECUTION_PERIOD_MILLISEC,
+                        TimeUnit.MILLISECONDS
+                );
+    }
+
+    /**
+     * Getter of the Ping instance
+     * @return the Ping singleton instance
+     */
+    public static Ping getInstance() {
+        shared = shared == null ? new Ping() : shared;
+        return shared;
+    }
+
+    /**
+     * Convenience method used to add a new connection to the pingHandler collection
+     * @param connection the connection that has to be added
+     */
+    public void addPing(Pingable connection) {
+        AdrenalineLogger.info("Added to ping: " + connection.hashCode());
+        getPingHandler().put(connection, true);
+    }
+
+    /**
+     * Convenience method used to notify a PONG message
+     * @param connectionID the hashCode of the connection
+     */
+    public void didPong(int connectionID) {
+        getPingHandler().keySet().forEach( connection -> {
+            if (connection.hashCode() == connectionID) {
+                getPingHandler().replace(connection, true);
+            }
+        });
+    }
+
+    /**
+     * Convenience method used to remove and close a connection to the pingHandler collection
+     * @param connection the connection that has to be closed
+     */
+    private void remove(Pingable connection) {
+        getPingHandler().remove(connection);
+        connection.closeConnection();
+    }
+
+    /**
+     * Getter of the pingHandler collection
+     * @return the pingHandler instance
+     */
+    private ConcurrentMap<Pingable, Boolean> getPingHandler() { return this.pingHandler; }
+}
---
+++ src/main/java/it/polimi/ingsw/utility/Pingable.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.utility;
+
+/**
+ * Interface used to make a ServerSocketClientConnection
+ * Ping methods compliant, in order to ping client connections.
+ *
+ * @author Stefano Formicola
+ */
+public interface Pingable {
+
+    /**
+     * The ServerSocketClientConnection calling this method will
+     * send a PING message to the client when asked.
+     */
+    void ping();
+
+    /**
+     * The Ping handler class will call this method if the
+     * client does not respond to a PING message after a
+     * determined delay.
+     */
+    void closeConnection();
+}
