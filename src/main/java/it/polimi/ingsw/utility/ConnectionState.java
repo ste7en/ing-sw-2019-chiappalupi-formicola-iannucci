0f2623129408e18ac02594a3497ae2c3d7fd5c4f
Stefano Formicola <stefano_17sf@libero.it>
2019-05-19 20:03:21 +0200

    ConnectionState enum

---
+++ src/main/java/it/polimi/ingsw/utility/ConnectionState.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.utility;
+
+/**
+ * Enum representing in which states the connection can be
+ *
+ * @author Stefano Formicola
+ */
+
+public enum ConnectionState {
+    ONLINE,
+    CLOSED
+}
