82774d96a240c061b2bc53038a5c6431b0593649
Stefano Formicola <stefano_17sf@libero.it>
2019-05-19 20:02:08 +0200

    little style fixes for the logger's console formatter

--- src/main/java/it/polimi/ingsw/utility/AdrenalineLoggerConsoleFormatter.java
+++ src/main/java/it/polimi/ingsw/utility/AdrenalineLoggerConsoleFormatter.java
@@ -5,14 +5,19 @@ import java.text.SimpleDateFormat;
 import java.util.logging.*;
 
 /**
- * Simple plain text formatter for logging.
+ * Text formatter used when logging to the console
  *
  * @author Stefano Formicola
  */
 public class AdrenalineLoggerConsoleFormatter extends Formatter {
 
+    /**
+     * Private constants used when writing a log record to string
+     */
     private static final String space = " ";
     private static final String dash = "-";
+    private static final String newline = "\n";
+
     /**
      * Format the given log record and return the formatted string.
      * <p>
@@ -34,14 +39,25 @@ public class AdrenalineLoggerConsoleFormatter extends Formatter {
                 .concat(dash)
                 .concat(space)
                 .concat(record.getMessage())
+                .concat(newline)
                 ;
     }
 
+    /**
+     * Returns a formatted string based on the record level
+     * @param record a log record
+     * @return formatted string
+     */
     private String formattedLevel(LogRecord record) {
         var level = record.getLevel().getName();
-        return "[" + level + "]";
+        return "[" + level + "]"+space.repeat(7-level.length());
     }
 
+    /**
+     * Returns a specific ANSI color code for each record level
+     * @param level a record level
+     * @return ANSI color code as String
+     */
     private String ANSIFromRecordLevel(Level level) {
         switch (level.getName()) {
             case "CONFIG": return ANSI_COLORS.ANSI_BRIGHT_BLUE.value();
@@ -53,9 +69,14 @@ public class AdrenalineLoggerConsoleFormatter extends Formatter {
         }
     }
 
+    /**
+     * A simple string formatted date of the record
+     * @param record log record
+     * @return date as string
+     */
     private String formattedDate(LogRecord record) {
         var date = Date.from(record.getInstant());
-        return new SimpleDateFormat("H:mm:ss.S")
+        return new SimpleDateFormat("H:mm:ss.SSS")
                 .format(date);
     }
 
@@ -64,6 +85,9 @@ public class AdrenalineLoggerConsoleFormatter extends Formatter {
      */
     @SuppressWarnings("unused")
     private enum ANSI_COLORS {
+        /**
+         * Constants
+         */
         ANSI_RESET("\u001B[0m"),
         ANSI_BLACK("\u001B[30m"),
         ANSI_RED("\u001B[31m"),
@@ -82,11 +106,21 @@ public class AdrenalineLoggerConsoleFormatter extends Formatter {
         ANSI_BRIGHT_CYAN("\u001B[96m"),
         ANSI_BRIGHT_WHITE("\u001B[97m");
 
+        /**
+         * private value of each constant
+         */
         private String value;
 
-        // Constructor
+        /**
+         * Constructor
+         * @param s the ANSI color code
+         */
         ANSI_COLORS(String s) { value = s; }
 
+        /**
+         * Getter for the ANSI color code
+         * @return the ANSI value
+         */
         String value() { return value; }
     }
 }
