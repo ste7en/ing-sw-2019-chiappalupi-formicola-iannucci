7c50b48fa11f4dc430e549a7c63dfc79ffd7af0b
Elena Iannucci <elena.iannucci@mail.polimi.it>
2019-05-22 19:41:33 +0200

    wip: added work division

--- src/main/java/it/polimi/ingsw/view/View.java
+++ src/main/java/it/polimi/ingsw/view/View.java
@@ -22,73 +22,178 @@ public abstract class View implements Observer{
 
     public abstract void onFailure();
 
+    /**
+     * Ste
+     */
     public abstract void willChooseConnection();
 
+    /**
+     * Ste
+     */
     public abstract void didChooseConnection();
 
+    /**
+     * Ste
+     */
     public abstract void login();
 
+    /**
+     * Ste
+     */
     public abstract void onLoginFailure();
 
+    /**
+     * Ste
+     */
     public abstract void onLoginSuccess();
 
+    /**
+     * Ste
+     */
     public abstract void joinWaitingRoom();
 
+    /**
+     * Ste
+     */
     public abstract void didJoinWaitingRoom();
 
+    /**
+     * Ele
+     */
     public abstract void willChooseCharacter();
 
+    /**
+     * Ele
+     */
     public abstract void didChooseCharacter();
 
+    /**
+     * Ele
+     */
     public abstract void willChooseGameSettings();
 
+    /**
+     * Ele
+     */
     public abstract void didChooseGameSettings();
 
+    /**
+     * Ele
+     */
     public abstract void willChooseSpawnPoint();
 
+    /**
+     * Ele
+     */
     public abstract void didChooseSpawnPoint();
 
+    /**
+     * Ele
+     */
     public abstract void onMove();
 
+    /**
+     * Ele
+     */
     public abstract void willChooseMovement();
 
+    /**
+     * Ele
+     */
     public abstract void didChooseMovement();
 
+    /**
+     * Ele
+     */
     public abstract void willChooseWhatToGrab();
 
+    /**
+     * Ele
+     */
     public abstract void didChooseWhatToGrab();
 
+    /**
+     * Dani
+     */
     public abstract void willChooseWeapon();
 
+    /**
+     * Dani
+     */
     public abstract void didChooseWeapon();
 
+    /**
+     * Dani
+     */
     public abstract void willChooseDamage();
 
+    /**
+     * Dani
+     */
     public abstract void didChooseDamage();
 
+    /**
+     * Dani
+     */
     public abstract void willChooseMode();
 
+    /**
+     * Dani
+     */
     public abstract void didChooseMode();
 
+    /**
+     * Dani
+     */
     public abstract void willChooseEffects();
 
+    /**
+     * Dani
+     */
     public abstract void didChooseEffects();
 
+    /**
+     * Ste
+     */
     public abstract void onEndTurn();
 
+    /**
+     * Dani
+     */
     public abstract void willReload();
 
+    /**
+     * Dani
+     */
     public abstract void didReload();
 
+    /**
+     * Ste
+     */
     public abstract void willUsePowerup();
 
+    /**
+     * Ste
+     */
     public abstract void didUsePowerup();
 
+    /**
+     * Ste
+     */
     public abstract void willChoosePowerup();
 
+    /**
+     * Ste
+     */
     public abstract void didChoosePowerup();
 
+    /**
+     * Ste
+     */
     public abstract void willChoosePowerupEffect();
 
+    /**
+     * Ste
+     */
     public abstract void didChoosePowerupEffect();
 }
