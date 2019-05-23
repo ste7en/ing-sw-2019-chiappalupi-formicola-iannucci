94a47fc49490a4987ea7c3738a3d764b62505bd4
Elena Iannucci <elena.iannucci@mail.polimi.it>
2019-05-23 19:49:31 +0200

    Updated javaFXApp class

--- src/main/java/it/polimi/ingsw/JavaFXApp.java
+++ src/main/java/it/polimi/ingsw/JavaFXApp.java
@@ -2,20 +2,13 @@ package it.polimi.ingsw;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.Group;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class JavaFXApp extends Application {
@@ -26,73 +19,61 @@ public class JavaFXApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Button test");
-        Button buttonRMI = new Button();
-        buttonRMI.setText("RMI");
-        Button buttonTCP = new Button();
-        buttonTCP.setText("TCP");
+        CheckBox checkBoxRMI = new CheckBox("RMI");
+        CheckBox checkBoxTCP = new CheckBox("TCP");
+        Button button = new Button("Continue");
+        Label label = new Label("Select a connection");
+        button.setOnAction(e -> handleOptions(checkBoxRMI, checkBoxTCP));
+        checkBoxRMI.setOnAction(e -> handleOptionsRMI(checkBoxRMI, checkBoxTCP));
+        checkBoxTCP.setOnAction(e -> handleOptionsTCP(checkBoxRMI, checkBoxTCP));
+        BorderPane root = new BorderPane();
 
-        /*
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-        Scene scene = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();*/
+        HBox boxRMI = new HBox(checkBoxRMI);
+        boxRMI.setAlignment(Pos.CENTER);
 
-        BorderPane root = new BorderPane(new Label("Choose your connection!"));
+        HBox boxTCP = new HBox(checkBoxTCP);
+        boxTCP.setAlignment(Pos.CENTER);
 
-        HBox boxRMI = new HBox(buttonRMI);
-        boxRMI.setAlignment(Pos.CENTER_LEFT);
-        HBox.setHgrow(boxRMI, Priority.ALWAYS);
+        HBox boxButton = new HBox(button);
+        boxButton.setAlignment(Pos.CENTER);
+        boxButton.setMargin(button, new Insets(0, 0, 50, 0));
 
-        HBox boxTCP = new HBox(buttonTCP);
-        boxTCP.setAlignment(Pos.CENTER_RIGHT);
-        HBox.setHgrow(boxTCP, Priority.ALWAYS);
+        HBox boxLabel = new HBox(label);
+        boxLabel.setAlignment(Pos.CENTER);
+        boxLabel.setMargin(label, new Insets(50, 0, 0, 0));
 
-        HBox bottom = new HBox(boxRMI, boxTCP);
-        bottom.setPadding(new Insets(10));
-        root.setBottom(bottom);
+        HBox center = new HBox(boxRMI, boxTCP);
+        center.setAlignment(Pos.CENTER);
+        center.setSpacing(150);
+        root.setCenter(center);
+        root.setBottom(boxButton);
+        root.setTop(boxLabel);
 
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
-    /*public static void main(String[] args) {
-        launch(args);
+    public void handleOptions(CheckBox rmi, CheckBox tcp){
+        if(rmi.isSelected()){
+            System.out.println("Hai selezionato RMI!");
+        }
+        if(tcp.isSelected()){
+            System.out.println("Hai selezionato TCP!");
+        }
+        if(!tcp.isSelected() && !rmi.isSelected()){
+            System.out.println("Seleziona qualcosa!");
+        }
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Drawing Operations Test");
-        Group root = new Group();
-        Canvas canvas = new Canvas(300, 250);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        drawShapes(gc);
-        root.getChildren().add(canvas);
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    public void handleOptionsRMI(CheckBox rmi, CheckBox tcp){
+        if(rmi.isSelected()) {
+            tcp.setSelected(false);
+        }
     }
 
-    private void drawShapes(GraphicsContext gc) {
-        Button button = new Button("ciao");
-        gc.setFill(Color.GREEN);
-        gc.setStroke(Color.BLUE);
-        gc.setLineWidth(5);
-        gc.strokeLine(40, 10, 10, 40);
-        gc.fillOval(10, 60, 30, 30);
-        gc.strokeOval(60, 60, 30, 30);
-        gc.fillRoundRect(110, 60, 30, 30, 10, 10);
-        gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
-        gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
-        gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
-        gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
-        gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
-        gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
-        gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
-        gc.fillPolygon(new double[]{10, 40, 10, 40},
-                new double[]{210, 210, 240, 240}, 4);
-        gc.strokePolygon(new double[]{60, 90, 60, 90},
-                new double[]{210, 210, 240, 240}, 4);
-        gc.strokePolyline(new double[]{110, 140, 110, 140},
-                new double[]{210, 210, 240, 240}, 4);
-    }*/
+    public void handleOptionsTCP(CheckBox rmi, CheckBox tcp){
+        if(tcp.isSelected()) {
+            rmi.setSelected(false);
+        }
+    }
 }
