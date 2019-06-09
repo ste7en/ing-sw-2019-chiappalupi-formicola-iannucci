b6d7686ee6c161d89d15279e0151905da482282c
Stefano Formicola <stefano_17sf@libero.it>
2019-06-09 19:18:21 +0200

    Characters read from json

--- src/main/java/it/polimi/ingsw/model/player/Character.java
+++ src/main/java/it/polimi/ingsw/model/player/Character.java
@@ -1,6 +1,14 @@
 package it.polimi.ingsw.model.player;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import it.polimi.ingsw.model.utility.PlayerColor;
+import it.polimi.ingsw.utility.AdrenalineLogger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *
@@ -20,7 +28,25 @@ public class Character {
      */
     private static final String EMPTY_STRING_DESCRIPTION = "Cannot create a character with an empty string as description.";
 
+    /**
+     * Log string
+     */
+    private static final String ERR_CHARACTER_INIT = "An error has occurred while trying to read characters from json.";
 
+    /**
+     * Number of available characters for the game
+     */
+    private static final int NUM_CHARACTERS = 5;
+
+    /**
+     * Collection of private available characters
+     */
+    private static final ArrayList<Character> characters = initializeCharacters();
+
+    /**
+     * Character's name
+     */
+    private String name;
     /**
      * Color of the character
      */
@@ -32,25 +58,17 @@ public class Character {
     private String description;
 
     /**
-     * Character's name
+     * Public initializer only for tests
+     * @param name character name
+     * @param color character color
+     * @param description character description
      */
-    private String name;
+    public Character(String name, PlayerColor color, String description) {}
 
     /**
-     * Constructor: creates a new Character based on its name, color and description.
-     * @param name the name of the character
-     * @param color the color of the character, defined as a PlayerColor Enum value
-     * @param description the description of the character, as defined in the game's manual
-     * @throws IllegalArgumentException for empty strings
+     * Default constructor for json parsing
      */
-    public Character(String name, PlayerColor color, String description) {
-        if (name.isEmpty()) { throw new IllegalArgumentException(EMPTY_STRING_NAME); }
-        if (description.isEmpty()) { throw new IllegalArgumentException(EMPTY_STRING_DESCRIPTION); }
-
-        this.name = name;
-        this.color = color;
-        this.description = description;
-    }
+    private Character() {}
 
     /**
      * Returns the name of the character
@@ -74,6 +92,47 @@ public class Character {
      */
     public String getDescription() {
         return this.description;
+    }
+
+    /**
+     * Method that initializes the weapons deck using a json file
+     * @return an ArrayList containing the deck of weapons for testing purposes
+     */
+    private static ArrayList<Character> initializeCharacters() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Character[] box = new Character[NUM_CHARACTERS];
+        try {
+            File json = new File("src" + File.separator + "main" + File.separator + "resources" + File.separator + "characters.json");
+            box = objectMapper.readValue(json, Character[].class);
+
+        } catch (IOException e) {
+            AdrenalineLogger.errorException(ERR_CHARACTER_INIT, e);
+        }
+        return new ArrayList<>(Arrays.asList(box));
+    }
+
+    /**
+     * Getter of all the available characters
+     * @return a collection of all the available characters of the game
+     */
+    public static ArrayList<Character> getCharacters() { return (ArrayList<Character>)characters.clone(); }
+
+    /**
+     * Returns a Character from a given name, if exists
+     * @param name character name
+     * @return a character
+     */
+    public static Character getCharacterFromName(String name) {
+        return characters.stream().filter(character -> character.getName().equals(name)).findFirst().orElseThrow();
+    }
+
+    /**
+     * Returns a Character from a given color, if exists
+     * @param color character color
+     * @return a character
+     */
+    public static Character getCharacterFromColor(PlayerColor color) {
+        return characters.stream().filter(character -> character.getColor().equals(color)).findFirst().orElseThrow();
     }
 
 }
